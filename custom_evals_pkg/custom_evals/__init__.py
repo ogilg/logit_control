de264de03424a9a56dc055e009bd93e82c6598d6
Oscar Gilg <oscar.gilg18@gmail.com>
2025-07-30 15:46:18 +0000

    rename custom_evals folder

--- custom_evals_pkg/custom_evals/__init__.py
+++ custom_evals_pkg/custom_evals/__init__.py
@@ -1,12 +1,13 @@
 """Custom evaluations package - HuggingFace provider functionality."""
 
-from .huggingface_provider import generate_single_token, HUGGINGFACE_MODEL_MAPPING, huggingface_get_probs
+from .huggingface_provider import generate_single_token, HUGGINGFACE_MODEL_MAPPING, huggingface_get_probs, huggingface_get_text
 from .request_templates import GetTextRequest, GetTextResponse, GetProbsRequest, GetProbsResponse, Message, Prompt
 
 __version__ = "0.1.0"
 __all__ = [
     "generate_single_token", 
     "huggingface_get_probs",
+    "huggingface_get_text",
     "HUGGINGFACE_MODEL_MAPPING",
     "GetTextRequest",
     "GetTextResponse", 
